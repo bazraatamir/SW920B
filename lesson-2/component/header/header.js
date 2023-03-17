99d4de5e596aa2f2df152eabb8223f57b375664c
bazraatamir <69583471+bazraatamir@users.noreply.github.com>
2023-03-17 12:09:37 +0800

    router

---
+++ lesson-2/component/header/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from"./style.module.css";
+import { Link } from 'react-router-dom';
+
+function Header () {
+    return (  
+    <div className={style.header}>
+        <h1 >The blog</h1>
+        <div className={style.toolBar}>
+            <Link to="/">home</Link>
+            <Link to="/Create">Create</Link>
+        </div>
+        
+    </div>
+    );
+}
+
+export default Header;
